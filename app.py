2da653ec5862e2925a4186d623390139c879171a
Skateallday <marcinglis91@hotmail.com>
2019-10-11 13:41:03 +0100

    navigation set up, boostrap working

--- app.py
+++ app.py
@@ -4,9 +4,17 @@ app = Flask(__name__)
 application = app # our hosting requires application in passenger_wsgi
 
 @app.route("/")
+@app.route("/home")
 def home():
     return render_template("index.html")
 
+@app.route("/plant1")
+def plant1():
+    return render_template("plant1.html")
+
+@app.route("/plant2")
+def plant2():
+    return render_template("plant2.html")
 
 if __name__ == "__main__":
       app.run('localhost', 5000, debug=True)

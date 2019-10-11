328a8ac9d813088cbf874de537ba22d49c5f8f12
Skateallday <marcinglis91@hotmail.com>
2019-10-11 12:28:46 +0100

    second commit

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+application = app # our hosting requires application in passenger_wsgi
+
 @app.route("/")
 def home():
     return render_template("index.html")
